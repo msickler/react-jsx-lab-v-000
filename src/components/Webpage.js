0f015994b8fb501a1b3ba9d07de67bb3c816bcb3
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-29 18:49:26 +0000

    automatically backed up by learn

--- src/components/Webpage.js
+++ src/components/Webpage.js
@@ -5,7 +5,7 @@ class Webpage extends React.Component {
   render() {
     return(
       <div>
-        <title>"The world's coolest webpage"</title>
+        <title>The world's coolest webpage</title>
         <FillerText />
         <FillerText />
       </div>
