acf8281d24266dca11b137bb03ca5422ee0972f4
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-29 18:40:27 +0000

    automatically backed up by learn

---
+++ src/components/RegistrationForm.js
@@ -0,0 +1,12 @@
+class RegistrationForm extends React.Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <input type="text">
+          <input type="password">
+        </form>
+      </div>
+    )
+  }
+}
