04eb36019e5bcb5f61ae2dbaf6a983ad19f89639
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-29 18:42:32 +0000

    automatically backed up by learn

---
+++ src/components/FillerText.js
@@ -0,0 +1,9 @@
+class FillerText extends React.Component {
+  render() {
+    return(
+      <p> "I am a filler text. I can be used to fill your screen. Amazing!" </p>
+    )
+  }
+}
+
+export default FillerText 
